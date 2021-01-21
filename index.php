6afec833fcb1616e75d586c2012c3f4ecb10550f
Elton <eltonrazzini@hotmail.com>
2021-01-21 15:47:04 -0300

    responsivo

--- index.php
+++ index.php
@@ -4,26 +4,54 @@
 require 'layouts/header.php';
 require './admin/modelos/Noticia.php';
 
-$noticias = new Noticia();
+$get_dados = filter_input_array(INPUT_GET, FILTER_DEFAULT);
+$arrayNoticias = [];
 
+$noticias = new Noticia();
 $arrayNoticias = [];
 $todasNoticias = $noticias->listarNoticia();
 
 $arrayNoticias = $todasNoticias;
 
-?>
+if (isset($get_dados['id_acesso'])) {
 
+    $noticia = new Noticia();
+    $umaNoticia = $noticia->listarUmaNoticia($get_dados['id_acesso']);
+    $arrayNoticias = $umaNoticia;
+}
+if(isset($get_dados['filter'])){
+
+    $noticia = new Noticia();
+    $umaNoticia = $noticia->buscar($get_dados['filter']);
+    $arrayNoticias = $umaNoticia;
+}
+?>
+<link rel="stylesheet" href="./public/acessoNoticia.css">
 <?php foreach ($arrayNoticias as $noticia) { ?>
 
-<div class="container_cards">
-    <div class="cards">
-        <h1 class="card_item"><?php echo $noticia['titulo']; ?></h1>
-        <p class="card_item"> <?php echo $noticia['noticia']; ?></p>
-        <p class="card_item"> <?php echo $noticia['nome_categoria']; ?> </p>
-        <p class="card_item"> <?php echo $noticia['data_da_noticia']; ?></p>
-        <a href="./admin/AcessoNoticia.php?id_noticia=<?php echo $noticia['id_noticia']; ?>"><button class="card_btn-acesso">Acesso noticia</button></a>
+    <div class="container_cards">
+
+        <div class="cards">
+            <h1 class="card_item"><?php echo $noticia['titulo']; ?></h1>
+            <p class="card_item"> <?php echo $noticia['noticia']; ?></p>
+            <p class="card_item">categoria: <?php echo $noticia['nome_categoria']; ?> </p>
+            <p class="card_item">Data: <?php echo $noticia['data_da_noticia']; ?></p>
+
+            <?php if (isset($get_dados['id_acesso'])) { ?>
+
+                <a href="./admin/controllers/ControllerNoticia.php?delete_noticia=<?php echo $noticia['id_noticia']; ?>"><button class="card_btn-acesso">deletar</button></a>
+                <a href="./admin/CadastroNoticia.php?id_noticia=<?php echo $noticia['id_noticia']; ?>"><button class="card_btn-acesso">editar</button></a>
+
+            <?php
+            } else { ?>
+
+                <a href="index.php?id_acesso=<?php echo $noticia['id_noticia']; ?>"><button class="card_btn-acesso">Acesso noticia</button></a>
+
+            <?php
+            }
+            ?>
+        </div>
     </div>
-</div>
 
 
 <?php }
--- layouts/header.php
+++ layouts/header.php
@@ -17,19 +17,22 @@
     <div class="container">
 
         <header>
-            <ul class="header">
-                <li class="header-item header-item_page">
+            <div class="header">
+                <div class="header-item header-item_page">
                     <a href="../index.php"><img src="https://img.icons8.com/color/48/000000/news.png"/></a>
-                </li>
-                <li class="header-item header-item_page"><a href="../admin/CadastroCategoria.php">Cadastro Categoria</a></li>
-                <li class="header-item header-item_page"><a href="../admin/CadastroNoticia.php">Cadastro noticia</a></li>
-                <li class="header-item header-item_busca">
-                    <form action="../admin/AcessoNoticia.php" method="GET">
+                </div>
+                <div class="header-item header-item_page"><a href="../admin/CadastroCategoria.php">Cadastro Categoria</a></div>
+                <div class="header-item header-item_page"><a href="../admin/CadastroNoticia.php">Cadastro noticia</a></div>
+                <div class="header-item header-item_busca">
+                    <form  class="buscar" action="../index.php" method="GET">
                         <input type="text" name="filter" id="busca">
                         <input type="submit" id="lupa" value="buscar" />
                     </form>
-                </li>
-            </ul>
+                </div>
+                <div>
+                </div>
+
+            </div>
         </header>
 
         <main>
